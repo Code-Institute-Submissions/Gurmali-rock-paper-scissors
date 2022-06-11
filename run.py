3dc68107389647fab64143fd3824b91372fa719e
Gurmali <Gurmali@hotmail.com>
2022-06-11 18:14:12 +0000

    Add function to restart game

--- run.py
+++ run.py
@@ -3,16 +3,30 @@ import random
 
 def game():
     player = input("Make a choice:\n rock, paper or scissors?\n")
+    print(f"You chose {player}\n")
     ai = random.choice(['rock', 'paper', 'scissors'])
-    print(f"Computer chose {ai}")
+    print(f"Computer chose {ai}\n")
 
     if player == ai:
-        return "Its a tie"
+        return "Its a tie.\n"
     
     if winner(player, ai):
-        return "You win! :)"
+        return "You win! :)\n"
     else:
-        return "Computer won :("
+        return "Computer won :(\n"
+    
+
+def replay():
+    play_again = input("Do you want to play again?  y/n\n")
+
+    if play_again == "y":
+        print(game())
+        replay()
+    elif play_again == "n":
+        None
+    else:
+        print(f"{play_again} is not an option, try again!\n")
+        replay()
 
 
 def winner(user, computer):
@@ -23,3 +37,4 @@ def winner(user, computer):
 
 
 print(game())
+print(replay())
