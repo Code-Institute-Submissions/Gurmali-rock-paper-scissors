ad6a59648661c2a79654f5f17706749a57b53dc7
Gurmali <Gurmali@hotmail.com>
2022-06-12 09:53:25 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
 import random
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('played')
+
+Blad1 = SHEET.worksheet('Blad1')
+
+data = Blad1.get_all_values()
+
+print(data)
 
 
 def game():
@@ -31,6 +51,7 @@ def replay():
     play_again = input("Do you want to play again?  y/n\n")
 
     if play_again == "y":
+        print(data)
         print(game())
         replay()
     elif play_again == "n":
